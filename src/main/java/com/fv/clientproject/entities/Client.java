7f9a47ac7963a217690db3b229e3ab2d0cf79c1a
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-06-28 09:23:59 -0300

    Projeto criado

---
+++ src/main/java/com/fv/clientproject/entities/Client.java
@@ -0,0 +1,11 @@
+package com.fv.clientproject.entities;
+
+import java.io.Serializable;
+
+public class Client implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+}
